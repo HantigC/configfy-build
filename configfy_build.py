843b265ef1e483e0e614b5ecf5caac34707ffa35
HantigC <cristianhantig96@gmail.com>
2023-03-12 22:05:39 +0200

    add load function dynamically

--- configfy_build.py
+++ configfy_build.py
@@ -1,5 +1,5 @@
-from copy import deepcopy
-from typing import Any, Dict, List
+import importlib
+from typing import Any, Dict, List, Callable
 
 
 class BuildConfigException(Exception):
@@ -9,56 +9,57 @@ class BuildConfigException(Exception):
 
 
 class Build:
-    def __init__(self, function, typename=None, config=None, name=None):
-        self.function = function
-        self.config = config
-        self.typename = typename
+    def __init__(self, function_name, context, scope, name):
+        self.function_name = function_name
+        self.scope = scope
         self.name = name
+        self.context = context
+        self.build_function = None
         self._built_instance = None
 
     def build(self, config=None):
-        return self.function(config or self.config)
+        if self.build_function is None:
+            self.build_function = _load_function_dyn(self.function_name)
+        return self.build_function(config or self.scope)
 
-    def buil_once(self, config=None):
+    def build_once(self, config=None):
         if self._built_instance is None:
             self._built_instance = self.build(config)
         return self._built_instance
 
+    def build_component(self, component_name):
+        return self.context.build(self.scope[component_name])
+
+    @classmethod
+    def from_scope(cls, scope, context):
+        return cls(
+            function_name=scope["function_name"],
+            context=context,
+            scope=scope,
+            name=scope["name"],
+        )
+
 
 class ConfigBuild:
     def __init__(self):
-        self.name_registry: Dict[str, Build] = {}
-        self.typename_registry: Dict[str, Build] = {}
+        self._registry: Dict[str, Build] = {}
         self.entrypoint = None
 
-    def register(self, typename):
-        def wrapper(wrapee):
-            self.typename_registry[typename] = Build(function=wrapee, typename=typename)
-            return wrapee
+    def build_one(self, name) -> Any:
+        return self.get(name).build_once()
 
-        return wrapper
+    def build_entrypoint(self):
+        return self.build(self.entrypoint)
 
-    def build_by_typename(self, typename: str) -> Any:
-        return self.get_by_typename(typename).buil_once()
+    def build(self, name: str) -> Any:
+        return self.get(name).build()
 
-    def get_by_typename(self, typename: str) -> Build:
+    def get(self, name: str) -> Build:
         try:
-            builder = self.typename_registry[typename]
+            builder = self.typename_registry[name]
         except KeyError as e:
             raise BuildConfigException(
-                f"typename `{typename}` should be found in the registry"
-            ) from e
-        return builder
-
-    def build_by_name(self, name: str) -> Any:
-        return self.get_by_name(name).buil_once()
-
-    def get_by_name(self, name: str) -> Build:
-        try:
-            builder = self.name_registry[name]
-        except KeyError as e:
-            raise BuildConfigException(
-                f"name `{name}` should be found in the registry"
+                f"typename `{name}` should be found in the registry"
             ) from e
         return builder
 
@@ -67,32 +68,31 @@ class ConfigBuild:
             components: List[Dict[str, Any]] = config_dict["components"]
         except KeyError as e:
             raise BuildConfigException(
-                "`components` field missing form the config"
+                "`components` field missing from the config"
             ) from e
 
         for component_config in components:
-            name = _get_field(component_config, "name")
-            self.name_registry[name] = self.make_builder(component_config)
+            builder = Build.from_scope(component_config, self)
+            self._registry[builder.name] = builder
 
-    def make_builder(self, config):
-        builder = self.get_by_typename(config["typename"])
-        builder = deepcopy(builder)
-        builder.config = config
-        builder.name = config["name"]
-        return builder
+        self.entrypoint = config_dict["entrypoint"]
 
-    def load_from_json(self, json_filename: str) -> None:
+    def load_json(self, json_filename: str) -> None:
         import json
 
         with open(json_filename, "r") as json_file:
             config_dict = json.load(json_file)
         self.load_dict(config_dict)
 
+    @classmethod
+    def from_json(cls, json_filename: str) -> "ConfigBuild":
+        obj = cls()
+        obj.load_json(json_filename)
+        return obj
 
-def _get_field(dict_: Dict[str, Any], field_name: str) -> Any:
-    try:
-        field = dict_[field_name]
-    except KeyError as e:
-        raise BuildConfigException() from e
-    else:
-        return field
+
+def _load_function_dyn(function_path: str) -> Callable[[Build], Any]:
+    from_where, function_name = function_path.rsplit(".", 1)
+    module = importlib.import_module(from_where)
+    function = getattr(module, function_name)
+    return function
